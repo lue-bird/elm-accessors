84d52f14c265edb0f2ca26545a09090992995757
lue-bird <lue.the.bird@gmail.com>
2022-06-04 23:40:37 +0200

    laws (rename, reformat)

--- tests/Laws.elm
+++ tests/Laws.elm
@@ -173,21 +173,21 @@ isSettable settable structureFuzzer alterFuzzer focusFuzzer =
             "identity"
             (\structure ->
                 structure
-                    |> setter_identity settable
+                    |> mapIdentityIsIdentity settable
                     |> Expect.equal True
             )
         , Test.fuzz
             (Fuzz.tuple3 ( structureFuzzer, alterFuzzer, alterFuzzer ))
             "composition"
             (\( structure, alter0, alter1 ) ->
-                setter_composition settable structure alter0 alter1
+                mapMultipleIsMapComposition settable structure alter0 alter1
                     |> Expect.equal True
             )
         , Test.fuzz
             (Fuzz.tuple3 ( structureFuzzer, focusFuzzer, focusFuzzer ))
             "set_set"
             (\( structure, focus0, focus1 ) ->
-                setter_set_set settable structure focus0 focus1
+                setMultipleIsSetLast settable structure focus0 focus1
                     |> Expect.equal True
             )
         ]
@@ -216,66 +216,82 @@ isLens settable structureFuzzer alterFuzzer focusFuzzer =
             "lens_set_get"
             (\fuzzed ->
                 fuzzed
-                    |> lens_set_get settable
+                    |> lensSetAsItsFocus settable
                     |> Expect.equal True
             )
         , Test.fuzz
             (Fuzz.tuple ( structureFuzzer, focusFuzzer ))
             "lens_get_set"
             (\( structure, focus ) ->
-                lens_get_set settable structure focus
+                lensSetFocusViewIsFocus settable structure focus
                     |> Expect.equal True
             )
         ]
 
 
-setter_identity :
+mapIdentityIsIdentity :
     TraversalConsume structure focus structure focus focusView focusView focusViewNamed
     -> structure
     -> Bool
-setter_identity settable structure =
-    (structure |> Accessor.mapOver settable identity) == structure
-
-
-setter_composition :
-    TraversalConsume structure focus structure focus focusView focusView focusViewNamed
-    -> structure
-    -> Alter focus
-    -> Alter focus
-    -> Bool
-setter_composition settable structure alter0 alter1 =
+mapIdentityIsIdentity settable structure =
     (structure
-        |> Accessor.mapOver settable alter1
-        |> Accessor.mapOver settable alter0
-    )
-        == (structure |> Accessor.mapOver settable (alter0 << alter1))
-
-
-setter_set_set :
-    TraversalConsume structure focus structure focus focusView focusView focusViewNamed
-    -> structure
-    -> focus
-    -> focus
-    -> Bool
-setter_set_set settable structure a b =
-    (structure
-        |> Accessor.mapOver settable (\_ -> a)
-        |> Accessor.mapOver settable (\_ -> b)
-    )
-        == (structure |> Accessor.mapOver settable (\_ -> b))
-
-
-lens_set_get : LensConsume structure focus structure focus focusNamed -> structure -> Bool
-lens_set_get lens_ structure =
-    (structure
-        |> Accessor.mapOver lens_
-            (\_ -> structure |> Accessor.view lens_)
+        |> Accessor.mapOver settable identity
     )
         == structure
 
 
-lens_get_set : LensConsume structure focus structure focus focusNamed -> structure -> focus -> Bool
-lens_get_set lens_ structure focus =
+mapMultipleIsMapComposition :
+    TraversalConsume structure focus structure focus focusView focusView focusViewNamed
+    -> structure
+    -> Alter focus
+    -> Alter focus
+    -> Bool
+mapMultipleIsMapComposition settable structure alter0 alter1 =
+    (structure
+        |> Accessor.mapOver settable alter0
+        |> Accessor.mapOver settable alter1
+    )
+        == (structure
+                |> Accessor.mapOver settable (alter0 >> alter1)
+           )
+
+
+setMultipleIsSetLast :
+    TraversalConsume structure focus structure focus focusView focusView focusViewNamed
+    -> structure
+    -> focus
+    -> focus
+    -> Bool
+setMultipleIsSetLast settable structure focus0 focus1 =
+    (structure
+        |> Accessor.mapOver settable (\_ -> focus0)
+        |> Accessor.mapOver settable (\_ -> focus1)
+    )
+        == (structure
+                |> Accessor.mapOver settable (\_ -> focus1)
+           )
+
+
+lensSetAsItsFocus :
+    LensConsume structure focus structure focus focusNamed
+    -> structure
+    -> Bool
+lensSetAsItsFocus lens_ structure =
+    (structure
+        |> Accessor.mapOver lens_
+            (\_ ->
+                structure |> Accessor.view lens_
+            )
+    )
+        == structure
+
+
+lensSetFocusViewIsFocus :
+    LensConsume structure focus structure focus focusNamed
+    -> structure
+    -> focus
+    -> Bool
+lensSetFocusViewIsFocus lens_ structure focus =
     (structure
         |> Accessor.mapOver lens_ (\_ -> focus)
         |> Accessor.view lens_
